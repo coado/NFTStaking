e6cdca0c27be687ce97aa5b23b0ac70e09a4b103
coado <dawid.malecki21@gmail.com>
2022-02-26 17:06:31 +0100

    built tests

--- contracts/Logic.sol
+++ contracts/Logic.sol
@@ -2,19 +2,26 @@
 pragma solidity ^0.8.11;
 
 import "./ERC721.sol";
+import "./ERC20.sol";
+import "hardhat/console.sol";
 
-contract LogicContract {
+contract LogicContract is ERC20 {
     event NewNFTInStaking(address sender, uint tokenId);
     event UnstakeNFT(address sender, uint tokenId);
 
     address public NFTStakingContract;
 
     bool public initialized = false;
+    
+    uint public stakingPeriod = 365 days;
+    uint public stakingRewardsPerNFT = 1000;
 
     // user address => token id => start staking time
     mapping (address => mapping(uint => uint)) public userStakingData;
     // user address => array of all stake user tokens 
     mapping (address => uint[]) public userStakingTokens;
+
+
 
     function init(address _address) public {
         require(!initialized, "Contract has been already initialized");
@@ -24,6 +31,7 @@ contract LogicContract {
     }
 
     function stakeNFT(uint _tokenId) external {
+        uint startGas = gasleft();
         // There is no necessity to check if token is already staked, because
         // even if is, it wouldn't be transfered to this contract
         require(_tokenId != 0, "Token id is equal to 0");
@@ -36,10 +44,13 @@ contract LogicContract {
         nftContract.transferFrom(msg.sender, address(this), _tokenId);
 
         emit NewNFTInStaking(msg.sender, _tokenId);
+        console.log(startGas - gasleft());
     }  
 
     function unStakeNFT(uint _tokenId) external {
         require(userStakingData[msg.sender][_tokenId] != 0, "NFT is not staking");
+        uint endStakingTimestamp = userStakingData[msg.sender][_tokenId] + stakingPeriod;
+        require(endStakingTimestamp < block.timestamp, "Your NFT is still in staking");
 
         delete userStakingData[msg.sender][_tokenId];
         
@@ -62,7 +73,16 @@ contract LogicContract {
 
         nftContract.transferFrom(address(this), msg.sender, _tokenId);
 
+        mint(stakingRewardsPerNFT);
+
         emit UnstakeNFT(msg.sender, _tokenId);
+
+    }
+
+     function mint(uint amount) private {
+        balanceOf[msg.sender] += amount;
+        totalSupply += amount;
+        emit Transfer(address(0), msg.sender, amount);
     }
 
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,5 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import { expect, should } from "chai";
+import { expect, should, assert } from "chai";
 import { ethers } from "hardhat";
 import { LogicContract, LogicContract__factory, NFT, NFT__factory, StakingFactory, StakingFactory__factory } from "../typechain";
 import { _abi } from '../typechain/factories/LogicContract__factory';
@@ -15,7 +15,8 @@ describe("Main Functionality", function () {
       stakingContractAddress1: string,
       stakingContractAddress2: string,
       stakingContract1: LogicContract,
-      stakingContract2: LogicContract
+      stakingContract2: LogicContract,
+      tokenId: number
 
   before(async () => {
     [owner, acc1, acc2] = await ethers.getSigners();
@@ -33,6 +34,8 @@ describe("Main Functionality", function () {
     // connecting with proxy contracts 
     stakingContract1 = new ethers.Contract(stakingContractAddress1, _abi, owner) as LogicContract
     stakingContract2 = new ethers.Contract(stakingContractAddress2, _abi, owner) as LogicContract
+
+    tokenId = 1
   })
 
   describe("Initial Tests", () => {
@@ -63,19 +66,63 @@ describe("Main Functionality", function () {
   describe("Staking NFT", () => {
 
     it("StakeNFT function", async () => {
-      await nftContract1.mint(acc1.address, 1);
-      await nftContract1.connect(acc1).approve(stakingContract1.address, 1)
+      await nftContract1.mint(acc1.address, tokenId)
+      await nftContract1.connect(acc1).approve(stakingContract1.address, tokenId)
 
       // calling with tokenId: 0
       await expect(stakingContract1.connect(acc1).stakeNFT(0)).to.be.reverted
       // calling with tokenId that user doesn't approve / have
       await expect(stakingContract1.connect(acc1).stakeNFT(10)).to.be.reverted
 
-      await stakingContract1.connect(acc1).stakeNFT(1)
+      await stakingContract1.connect(acc1).stakeNFT(tokenId)
+
+      // trying to stake the same token twice
+      await expect(stakingContract1.connect(acc1).stakeNFT(tokenId)).to.be.reverted
+
+      // checking timestamp for bad tokenId
+      let timestamp = await stakingContract1.connect(acc1).userStakingData(acc1.address, 2);
                  
-      await expect(stakingContract1.connect(acc1).userStakingData(acc1.address, 1)).to.exist
+      assert.equal(Object.values(timestamp)[0], '0x00', 'block.timestamp should be equal to 0')      
 
+      // checking timestamp for existing tokenId
+      timestamp = await stakingContract1.connect(acc1).userStakingData(acc1.address, tokenId);
 
+      assert.notEqual(Object.values(timestamp)[0], '0x00', 'block.timestamp shouldnt be equal to 0')      
+
+      const userTokens = await stakingContract1.userStakingTokens(acc1.address, 0)
+      assert.equal(Object.values(userTokens)[0], '0x01', `TokenId should be equal to ${tokenId}`)
+
+      // checking user tokenIds in other contract / function should be reverted
+      await expect(stakingContract2.userStakingTokens(acc1.address, 0)).to.be.reverted
+      
+      
+    })
+
+    it("UnstakeNFT function with high stake period time", async () => {
+      await expect(stakingContract1.connect(acc1).unStakeNFT(tokenId)).to.be.reverted
+    })
+
+    xit("UnstakeNFT function", async () => {
+      // bad tokenId
+      await expect(stakingContract1.connect(acc1).unStakeNFT(10)).to.be.reverted
+      // token doesnt belong to provided address
+      await expect(stakingContract1.connect(acc2).unStakeNFT(tokenId)).to.be.reverted
+      // bad tokenId
+      await expect(stakingContract1.connect(acc1).unStakeNFT(0)).to.be.reverted
+      // bad tokenId
+      await expect(stakingContract1.connect(acc1).unStakeNFT(-10)).to.be.reverted
+
+      await stakingContract1.connect(acc1).unStakeNFT(tokenId)
+      // calling unstake function twice
+      await expect(stakingContract1.connect(acc1).unStakeNFT(tokenId)).to.be.reverted
+
+      const timestamp = await stakingContract1.connect(acc1).userStakingData(acc1.address, tokenId)
+      // checking timestamp / should be 0 after deleting data                  
+      assert.equal(Object.values(timestamp)[0], '0x00', 'block.timestamp should be equal to 0')  
+      
+      await expect(stakingContract1.userStakingTokens(acc1.address, 0)).to.be.reverted
+
+      assert.equal(await nftContract1.ownerOf(tokenId), acc1.address, 'After calling unstake function, owner of NFT should be changed')
 
     })
   })
